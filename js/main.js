a54e749badb94668e6b08b6d971616bf91694899
Dima Markus <dmi3000@gmail.com>
2015-07-14 04:22:48 -0400

    Create knewton application page

---
+++ js/main.js
@@ -0,0 +1,35 @@
+(function() {
+  'use strict';
+
+  $(document).ready(function(){
+    $('.logo-carousel').slick({
+        slidesToShow: 4,
+        infinite: true,
+        responsive: [
+		    {
+		      breakpoint: 520,
+		      settings: {
+		        slidesToShow: 3,
+		      }
+		    },
+		  ]
+    });
+  });
+
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+})();
+
